40c5b6514f066e31ff03a2b65250c1adafee8e97
Engels Souza <engels.franca@icloud.com>
2024-08-18 22:46:26 -0300

    concluido subnets

--- modules/groundwork/private-subnets.tf
+++ modules/groundwork/private-subnets.tf
@@ -27,3 +27,38 @@ resource "aws_route_table_association" "private-2a" {
   route_table_id = aws_route_table.private-internet-access-zone-a.id
 }
 
+resource "aws_route_table" "private-internet-access-zone-b" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gw-zone-b.id
+  }
+
+  tags = {
+    Name = format("private-route-table-%sb", var.vpc_region)
+  }
+}
+
+resource "aws_route_table_association" "private-2b" {
+  subnet_id      = "subnet-0488429af4eaa85a2"
+  route_table_id = aws_route_table.private-internet-access-zone-b.id
+}
+
+resource "aws_route_table" "private-internet-access-zone-c" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gw-zone-c.id
+  }
+
+  tags = {
+    Name = format("private-route-table-%sc", var.vpc_region)
+  }
+}
+
+resource "aws_route_table_association" "private-2c" {
+  subnet_id      = "subnet-06f189d8ced321995"
+  route_table_id = aws_route_table.private-internet-access-zone-c.id
+}
