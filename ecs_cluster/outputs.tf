fabb19fb48adbd245f05f4358bd7a9a67afa52a9
Engels Souza <engels.franca@icloud.com>
2024-09-08 21:06:08 -0300

    nada demais

--- ecs_cluster/outputs.tf
+++ ecs_cluster/outputs.tf
@@ -32,3 +32,15 @@ output "data_private_subnet_2c" {
   value     = module.ecs-cluster.data_private_subnet_2c
   sensitive = true
 }
+
+output "aws_lb_dns" {
+  value = module.ecs-cluster.aws_lb_dns
+}
+
+output "ssm_lb_arn_id" {
+  value = module.ecs-cluster.ssm_aws_lb_id
+}
+
+output "ssm_lb_listener_arn_id" {
+  value = module.ecs-cluster.ssm_aws_lb_listener_id
+}
